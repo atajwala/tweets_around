0d17391d78e1e2e991a9a35c52f706d2a7338334
Aslam Tajwala <atajwala@gmail.com>
2011-11-30 16:11:20 -0800

    fixed assets pipeline issue for production env

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module TweetsNearby
@@ -47,6 +47,9 @@ module TweetsNearby
     # Enable the asset pipeline
     config.assets.enabled = true
 
+		# Heroku requires this to be false
+		config.assets.initialize_on_precompile = false
+		
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
