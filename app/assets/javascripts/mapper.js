aa06c87750583e87b5a8dea08f1776e4aecbdaa2
Aslam Tajwala <atajwala@gmail.com>
2011-12-02 22:17:18 -0800

    prettied tweet window | now uses InfoBox instead of InfoWindow for Google Maps API

--- app/assets/javascripts/mapper.js
+++ app/assets/javascripts/mapper.js
@@ -57,14 +57,38 @@
 	});
 	markers.push(marker);
 	
-	var prtyDate = prettyDate(tweets[iterator].created_at);
-	var tweetLink = prettyTweet(tweets[iterator].text);	
+	var hipTweet = prettyTweet(tweets[iterator]);	
+	
+	var myOptions = {
+	                 content: hipTweet
+	                ,disableAutoPan: false
+	                ,maxWidth: 0
+	                ,pixelOffset: new google.maps.Size(-200, -210)
+	                ,zIndex: null
+	                ,boxStyle: { 
+	                  background: "#C9DFAF",
+	                  opacity: 0.9,
+	                  width: "450px",
+	                 }
+	                ,closeBoxMargin: "10px 2px 2px 2px"
+	                ,closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif"
+	                ,infoBoxClearance: new google.maps.Size(5, 5)
+	                ,isHidden: false
+	                ,pane: "floatPane"
+	                ,enableEventPropagation: false
+	        };
+
+	        var infowindow = new InfoBox(myOptions);
+	    
+	
+/*	
 	var infowindow = new google.maps.InfoWindow({
-	  content: "<u>"+"<img src=\""+tweets[iterator].user.profile_image_url_https+"\" />  "+"<b>"+tweets[iterator].user.screen_name + "</b>" + " says: </u> <br />" + "<i>"+tweetLink+"</i>"+"<br />"+"("+prtyDate+")",
+	  //content: "<div class=\"msgbox\">"+"<u>"+"<img src=\""+tweets[iterator].user.profile_image_url_https+"\" />  "+"<b>"+tweets[iterator].user.screen_name + "</b>" + " says: </u> <br />" + "<i>"+tweetLink+"</i>"+"<br />"+"("+prtyDate+")"+"</div>",
+	  content: hipTweet,
 	  title: "Tweet",
 	  size: new google.maps.Size(50,30)
 	});
-	
+*/	
 	google.maps.event.addListener(marker, 'click', function() {
 	  toggleBounce(marker, infowindow);
 	});
@@ -78,7 +102,7 @@
 	  infowindow.close(map, marker);
     } else {
       marker.setAnimation(google.maps.Animation.BOUNCE);
-      infowindow.setOptions();
+      //infowindow.setOptions();
 	  infowindow.open(map, marker);
     }
   }
@@ -88,13 +112,24 @@
     tweets.push(tweet);
   }
 
-  function prettyTweet(tweet) {
+  function prettyLink(tweet) {
 	var urlRegex = /https*\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/gi;
 	var url = tweet.match(urlRegex);
 	if (url == null) { return tweet; } 
 	url[0] = "<a href=\""+url[0]+"\" target=\"_blank\">"+url[0]+"</a>";
 	tweet = tweet.replace(urlRegex, url[0]);
 	return tweet;
+  }
+
+  function prettyTweet(tweet) {
+	var prtyDate = prettyDate(tweet.created_at);
+	var tweetLink = prettyLink(tweet.text);
+    var tweetText = "<div class=\"msgbox\">"+
+					"<img src=\""+tweet.user.profile_image_url_https+"\" />  "+
+					"<b><twtname>"+tweet.user.screen_name+"</twtname></b>"+
+					" says: <br /><br />" + "<i><twttext>"+tweetLink+"</twttext></i>"+
+					"<br />"+"<twttime>("+prtyDate+")</twttime>"+"</div>";
+    return tweetText;
   }
 
 (function($) {
