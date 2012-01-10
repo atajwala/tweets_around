b6bd57c0ba4709be151a37d5d6d9a82d591856b3
Aslam Tajwala <atajwala@gmail.com>
2012-01-09 17:33:41 -0800

    added some indentation

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -15,33 +15,33 @@ class DisplayController < ApplicationController
 =end
 
   def results
-		if(params[:criteria] == CRITERIA[0])
-			result = Geokit::Geocoders::GoogleGeocoder.geocode(params[:addr])
-			
-			if(result.success == false)
-				render :file => 'invalid_address.js.erb' and return		
-			end		
-			
-			latlng = result.ll.split(',')
-			@mytweets = Tweet.near(
-										"geo.coordinates" => 
-										[ latlng[0].to_f, latlng[1].to_f ]
-									).limit(params[:count].to_i)								
-		else
-			latlng = params[:addr].split(/[\s,\/]/)
-			puts latlng
-		  @mytweets = Tweet.near(
-										"geo.coordinates" => 
-										[ latlng[0].to_f, latlng[1].to_f ]
-									).limit(params[:count].to_i)
-		end
+    if(params[:criteria] == CRITERIA[0])
+      result = Geokit::Geocoders::GoogleGeocoder.geocode(params[:addr])
+
+      if(result.success == false)
+        render :file => 'invalid_address.js.erb' and return		
+      end
+
+      latlng = result.ll.split(',')
+      @mytweets = Tweet.near(
+                    "geo.coordinates" => 
+                    [ latlng[0].to_f, latlng[1].to_f ]
+                  ).limit(params[:count].to_i)								
+    else
+      latlng = params[:addr].split(/[\s,\/]/)
+      puts latlng
+      @mytweets = Tweet.near(
+                    "geo.coordinates" => 
+                    [ latlng[0].to_f, latlng[1].to_f ]
+                  ).limit(params[:count].to_i)
+    end
 		
 	  respond_to do |format|
 		  format.html
 		  format.js 
 		  format.json{
 			  render :json => @mytweets.to_json
-			}
+      }
 		end
   end
 
